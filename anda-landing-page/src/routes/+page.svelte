2c26e24cc316cbfe4bcc8b7501ee36fd9b1f036a
hallvaaw <hallvard_wahler@hotmail.com>
2023-02-25 21:25:46 +0100

    updated styling

--- anda-landing-page/src/routes/+page.svelte
+++ anda-landing-page/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav class="flex justify-center space-x-96 border border-b-2 py-4">
+    <a href="#">Download</a>
+    <a href="#">About</a>
+    <a href="#">Citation</a>
+</nav>
+<main class="grid grid-cols-2 bg-gray-200">
+    <div>
+        <h1 class="text-4xl">ANDA</h1>
+        <h2 class="text-2xl">Automated neuronal differentiation analysis</h2>
+        <p>A desktop application for automated image analysis of differentiating neuronal cells</p>
+    </div>
+    <div class="grid grid-cols-1">
+        <button class="bg-blue-300 hover:bg-blue-200">Linux</button>
+        <button class="bg-blue-300 hover:bg-blue-200">MacOS</button>
+        <button class="bg-blue-300 hover:bg-blue-200">Windows</button>
+    </div>
+</main>
