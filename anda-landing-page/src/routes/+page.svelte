de4dfaf2e686eda737cfb8dd26ea218fad5e4fb6
hallvaaw <hallvard_wahler@hotmail.com>
2023-02-26 22:19:06 +0100

    updated buttons and layout

--- anda-landing-page/src/routes/+page.svelte
+++ anda-landing-page/src/routes/+page.svelte
@@ -1,20 +1,18 @@
-<nav class="flex justify-center space-x-96 border border-b-2 py-4">
+<nav class="flex justify-center space-x-96 border border-b-2 py-4 text-2xl">
     <a href="#">Download</a>
     <a href="#">About</a>
     <a href="#">Citation</a>
 </nav>
-<main class="grid grid-cols-2 bg-gray-200">
-    <div>
+<main class="flex flex-row bg-gray-200 h-screen">
+    <div class="text-center">
         <h1 class="text-4xl">ANDA</h1>
         <h2 class="text-2xl">Automated neuronal differentiation analysis</h2>
         <p>A desktop application for automated image analysis of differentiating neuronal cells</p>
     </div>
-    <div class="grid grid-cols-1">
-        <div>
-            <h1>Download ANDA</h1>
-            <button class="grid grid-cols-2 bg-blue-600 text-2xl text-slate-100 items-center rounded-xl p-2 m-2 mx-8 hover:bg-blue-300"><img src="/images/linux-svgrepo-com.png" alt="Linux icon" width="50" height="50">Linux</button>
-            <button class="grid grid-cols-2 bg-blue-600 text-2xl text-slate-100 items-center rounded-xl p-2 m-2 mx-8 hover:bg-blue-300"><img src="/images/apple-svgrepo-com.png" alt="Apple icon" width="50" height="50">MacOS</button>
-            <button class="grid grid-cols-2 bg-blue-600 text-2xl text-slate-100 items-center rounded-xl p-2 m-2 mx-8 hover:bg-blue-300"><img src="/images/windows-svgrepo-com.png" alt="Windows icon" width="50" height="50">Windows</button>
-        </div>
+    <div class="flex flex-col text-center mx-64">
+            <h1 class="text-4xl text-center">Download ANDA</h1>
+            <button class="grid grid-cols-2 bg-blue-600 items-center rounded-xl p-4 m-3 hover:bg-blue-300"><img src="/images/linux-svgrepo-com.png" alt="Linux icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-12">Linux</p></button>
+            <button class="grid grid-cols-2 bg-blue-600 items-center rounded-xl p-4 m-3 hover:bg-blue-300"><img src="/images/apple-svgrepo-com.png" alt="Apple icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-12">MacOS</p></button>
+            <button class="grid grid-cols-2 bg-blue-600 items-center rounded-xl p-4 m-3 hover:bg-blue-300"><img src="/images/windows-svgrepo-com.png" alt="Windows icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-12">Windows</p></button>
     </div>
 </main>
