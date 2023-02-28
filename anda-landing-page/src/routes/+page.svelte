1c7103005cc6b2a8cc868e44abc8e62150234332
hallvaaw <hallvard_wahler@hotmail.com>
2023-03-01 00:02:20 +0100

    added footer

--- anda-landing-page/src/routes/+page.svelte
+++ anda-landing-page/src/routes/+page.svelte
@@ -1,20 +1,26 @@
 <nav class="flex justify-center border border-b-2 text-2xl font-bold">
     <ul class="flex flex-row space-x-96">
-        <li href="#" class="p-4 px-8 hover:bg-sky-500 active:bg-sky-700 hover:cursor-pointer hover:text-slate-100">Download</li>
-        <li href="#" class="p-4 px-8 hover:bg-sky-500 active:bg-sky-700 hover:cursor-pointer hover:text-slate-100">About</li>
-        <li href="#" class="p-4 px-8 hover:bg-sky-500 active:bg-sky-700 hover:cursor-pointer hover:text-slate-100">Citation</li>
+        <li href="#" class="p-4 px-8 hover:bg-sky-500 active:bg-sky-700 hover:cursor-pointer hover:text-slate-100 duration-150">Download</li>
+        <li href="#" class="p-4 px-8 hover:bg-sky-500 active:bg-sky-700 hover:cursor-pointer hover:text-slate-100 duration-150">About</li>
+        <li href="#" class="p-4 px-8 hover:bg-sky-500 active:bg-sky-700 hover:cursor-pointer hover:text-slate-100 duration-150">Citation</li>
     </ul>
 </nav>
-<main class="flex flex-row bg-slate-100 h-screen">
-    <div class="text-center w-3/5">
-        <h1 class="text-4xl">ANDA</h1>
-        <h2 class="text-2xl">Automated neuronal differentiation analysis</h2>
-        <p>A desktop application for automated image analysis of differentiating neuronal cells</p>
+<main class="flex flex-row bg-slate-100 pb-80">
+    <div class="text-center w-3/5 mt-12">
+        <h1 class="text-6xl mb-8">ANDA</h1>
+        <h2 class="text-2xl my-6">Automated neuronal differentiation analysis</h2>
+        <h3 class="text-xl">A desktop application for automated image analysis of differentiating neuronal cells.</h3>
     </div>
-    <div class="flex flex-col items-center w-2/5">
-        <h1 class="text-4xl">Download ANDA</h1>
-        <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 px-2 m-2 hover:bg-sky-500 active:bg-sky-700"><img src="/images/linux-svgrepo-com.png" alt="Linux icon" width="50" height="50" class="ml-6 mr-[86px]"><p class="text-2xl font-bold text-slate-100 mr-24">Linux</p></button>
-        <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 px-2 m-2 hover:bg-sky-500 active:bg-sky-700"><img src="/images/apple-svgrepo-com.png" alt="Apple icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-20">MacOS</p></button>
-        <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 px-[14px] m-2 hover:bg-sky-500 active:bg-sky-700"><img src="/images/windows-svgrepo-com.png" alt="Windows icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-12">Windows</p></button>
+    <div class="flex flex-col items-center w-2/5 mt-12">
+        <h1 class="text-4xl mb-6">Download ANDA</h1>
+        <div>
+            <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 px-2 m-4 duration-100 hover:bg-sky-500 active:bg-sky-700"><img src="/images/linux-svgrepo-com.png" alt="Linux icon" width="50" height="50" class="ml-6 mr-[86px]"><p class="text-2xl font-bold text-slate-100 mr-24">Linux</p></button>
+            <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 px-2 m-4 duration-100 hover:bg-sky-500 active:bg-sky-700"><img src="/images/apple-svgrepo-com.png" alt="Apple icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-20">MacOS</p></button>
+            <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 px-[14px] duration-100 m-4 hover:bg-sky-500 active:bg-sky-700"><img src="/images/windows-svgrepo-com.png" alt="Windows icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-12">Windows</p></button>
+        </div>
     </div>
 </main>
+<footer class="bg-slate-200">
+    <div class="container flex flex-row py-20 mx-auto">
+    </div>
+</footer>
