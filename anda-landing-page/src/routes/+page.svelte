5193fd98e19130ac042da68f9f20dde859997279
hallvaaw <hallvard_wahler@hotmail.com>
2023-02-28 22:36:55 +0100

    updated buttons and widths

--- anda-landing-page/src/routes/+page.svelte
+++ anda-landing-page/src/routes/+page.svelte
@@ -6,15 +6,15 @@
     </ul>
 </nav>
 <main class="flex flex-row bg-slate-100 h-screen">
-    <div class="text-center">
+    <div class="text-center w-3/5">
         <h1 class="text-4xl">ANDA</h1>
         <h2 class="text-2xl">Automated neuronal differentiation analysis</h2>
         <p>A desktop application for automated image analysis of differentiating neuronal cells</p>
     </div>
-    <div class="flex flex-col text-center mx-64">
-            <h1 class="text-4xl text-center">Download ANDA</h1>
-            <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 m-3 hover:bg-sky-500 active:bg-sky-700"><img src="/images/linux-svgrepo-com.png" alt="Linux icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-12">Linux</p></button>
-            <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 m-3 hover:bg-sky-500 active:bg-sky-700"><img src="/images/apple-svgrepo-com.png" alt="Apple icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-12">MacOS</p></button>
-            <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 m-3 hover:bg-sky-500 active:bg-sky-700"><img src="/images/windows-svgrepo-com.png" alt="Windows icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-12">Windows</p></button>
+    <div class="flex flex-col items-center w-2/5">
+        <h1 class="text-4xl">Download ANDA</h1>
+        <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 px-2 m-2 hover:bg-sky-500 active:bg-sky-700"><img src="/images/linux-svgrepo-com.png" alt="Linux icon" width="50" height="50" class="ml-6 mr-[86px]"><p class="text-2xl font-bold text-slate-100 mr-24">Linux</p></button>
+        <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 px-2 m-2 hover:bg-sky-500 active:bg-sky-700"><img src="/images/apple-svgrepo-com.png" alt="Apple icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-20">MacOS</p></button>
+        <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 px-[14px] m-2 hover:bg-sky-500 active:bg-sky-700"><img src="/images/windows-svgrepo-com.png" alt="Windows icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-12">Windows</p></button>
     </div>
 </main>
