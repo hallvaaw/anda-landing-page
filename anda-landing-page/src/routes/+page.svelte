8c53a8a22d8c25c5dcd60b3fa4f8c1d54e1a2609
hallvaaw <hallvard_wahler@hotmail.com>
2023-02-28 12:52:27 +0100

    updated buttons and navbar

--- anda-landing-page/src/routes/+page.svelte
+++ anda-landing-page/src/routes/+page.svelte
@@ -1,9 +1,11 @@
-<nav class="flex justify-center space-x-96 border border-b-2 py-4 text-2xl">
-    <a href="#">Download</a>
-    <a href="#">About</a>
-    <a href="#">Citation</a>
+<nav class="flex justify-center border border-b-2 text-2xl font-bold">
+    <ul class="flex flex-row space-x-96">
+        <li href="#" class="p-4 px-8 hover:bg-sky-500 active:bg-sky-700 hover:cursor-pointer hover:text-slate-100">Download</li>
+        <li href="#" class="p-4 px-8 hover:bg-sky-500 active:bg-sky-700 hover:cursor-pointer hover:text-slate-100">About</li>
+        <li href="#" class="p-4 px-8 hover:bg-sky-500 active:bg-sky-700 hover:cursor-pointer hover:text-slate-100">Citation</li>
+    </ul>
 </nav>
-<main class="flex flex-row bg-gray-200 h-screen">
+<main class="flex flex-row bg-slate-100 h-screen">
     <div class="text-center">
         <h1 class="text-4xl">ANDA</h1>
         <h2 class="text-2xl">Automated neuronal differentiation analysis</h2>
@@ -11,8 +13,8 @@
     </div>
     <div class="flex flex-col text-center mx-64">
             <h1 class="text-4xl text-center">Download ANDA</h1>
-            <button class="grid grid-cols-2 bg-blue-600 items-center rounded-xl p-4 m-3 hover:bg-blue-300"><img src="/images/linux-svgrepo-com.png" alt="Linux icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-12">Linux</p></button>
-            <button class="grid grid-cols-2 bg-blue-600 items-center rounded-xl p-4 m-3 hover:bg-blue-300"><img src="/images/apple-svgrepo-com.png" alt="Apple icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-12">MacOS</p></button>
-            <button class="grid grid-cols-2 bg-blue-600 items-center rounded-xl p-4 m-3 hover:bg-blue-300"><img src="/images/windows-svgrepo-com.png" alt="Windows icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-12">Windows</p></button>
+            <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 m-3 hover:bg-sky-500 active:bg-sky-700"><img src="/images/linux-svgrepo-com.png" alt="Linux icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-12">Linux</p></button>
+            <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 m-3 hover:bg-sky-500 active:bg-sky-700"><img src="/images/apple-svgrepo-com.png" alt="Apple icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-12">MacOS</p></button>
+            <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 m-3 hover:bg-sky-500 active:bg-sky-700"><img src="/images/windows-svgrepo-com.png" alt="Windows icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-12">Windows</p></button>
     </div>
 </main>
