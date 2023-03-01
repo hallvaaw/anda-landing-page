d4a7554cbc15778f92ba577ade059cffc11ee881
hallvaaw <hallvard_wahler@hotmail.com>
2023-03-01 22:45:40 +0100

    made download component

---
+++ anda-landing-page/src/routes/components/Download.svelte
@@ -0,0 +1,15 @@
+<main class="flex flex-row bg-slate-100 pb-80">
+    <div class="text-center w-3/5 mt-12">
+        <h1 class="text-6xl mb-8">ANDA</h1>
+        <h2 class="text-2xl my-6">Automated neuronal differentiation analysis</h2>
+        <h3 class="text-xl">A desktop application for automated image analysis of differentiating neuronal cells.</h3>
+    </div>
+    <div class="flex flex-col items-center w-2/5 mt-12">
+        <h1 class="text-4xl mb-6">Download ANDA</h1>
+        <div>
+            <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 px-2 m-4 duration-100 hover:bg-sky-500 active:bg-sky-700"><img src="/images/linux-svgrepo-com.png" alt="Linux icon" width="50" height="50" class="ml-6 mr-[86px]"><p class="text-2xl font-bold text-slate-100 mr-24">Linux</p></button>
+            <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 px-2 m-4 duration-100 hover:bg-sky-500 active:bg-sky-700"><img src="/images/apple-svgrepo-com.png" alt="Apple icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-20">MacOS</p></button>
+            <button class="grid grid-cols-2 bg-sky-600 items-center rounded-xl p-4 px-[14px] duration-100 m-4 hover:bg-sky-500 active:bg-sky-700"><img src="/images/windows-svgrepo-com.png" alt="Windows icon" width="50" height="50" class="ml-6"><p class="text-2xl font-bold text-slate-100 mr-12">Windows</p></button>
+        </div>
+    </div>
+</main>
