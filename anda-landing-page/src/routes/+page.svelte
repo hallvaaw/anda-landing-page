ecff68e09763159c30294fb9557ede50dad49df1
hallvaaw <hallvard_wahler@hotmail.com>
2023-03-09 19:36:11 +0100

    removed footer

--- anda-landing-page/src/routes/+page.svelte
+++ anda-landing-page/src/routes/+page.svelte
@@ -1,16 +1,13 @@
-<nav class="flex justify-center border border-b-2 text-2xl font-bold">
-    <ul class="flex flex-row space-x-96">
-        <li href="#" class="p-4 px-8 hover:bg-sky-500 active:bg-sky-700 hover:cursor-pointer hover:text-slate-100 duration-150">Download</li>
-        <li href="#" class="p-4 px-8 hover:bg-sky-500 active:bg-sky-700 hover:cursor-pointer hover:text-slate-100 duration-150">About</li>
-        <li href="#" class="p-4 px-8 hover:bg-sky-500 active:bg-sky-700 hover:cursor-pointer hover:text-slate-100 duration-150">Citation</li>
-    </ul>
-</nav>
-<Download/>
-<footer class="bg-slate-200">
-    <div class="container flex flex-row py-20 mx-auto">
-    </div>
-</footer>
-
+<main class="flex flex-col h-screen bg-slate-200">
+    <nav class="flex justify-center border border-b-2 text-2xl font-bold">
+        <ul class="flex flex-row space-x-96">
+            <li href="#" class="p-4 px-8 hover:bg-sky-500 active:bg-sky-700 hover:cursor-pointer hover:text-slate-100 duration-150">Download</li>
+            <li href="#" class="p-4 px-8 hover:bg-sky-500 active:bg-sky-700 hover:cursor-pointer hover:text-slate-100 duration-150">About</li>
+            <li href="#" class="p-4 px-8 hover:bg-sky-500 active:bg-sky-700 hover:cursor-pointer hover:text-slate-100 duration-150">Citation</li>
+        </ul>
+    </nav>
+    <Download/>
+</main>
 
 <script>
 import Download from './components/Download.svelte'
