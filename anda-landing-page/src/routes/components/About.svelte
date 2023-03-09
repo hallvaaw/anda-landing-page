11965e8b05569d80e69a1f12ca6aee1dea195b8f
hallvaaw <hallvard_wahler@hotmail.com>
2023-03-09 20:00:55 +0100

    added about component

---
+++ anda-landing-page/src/routes/components/About.svelte
@@ -0,0 +1,23 @@
+<main class="flex flex-col bg-slate-100 pb-80">
+    <div class="text-center">
+        <h1 class="text-6xl mb-8">About ANDA</h1>
+        <p class="text-xl">ANDA is a desktop application for automated image analysis of differentiating neuronal cells</p>
+        <h2>Main features:</h2>
+    </div>
+    <div>
+        <div class="flex flex-col xl:flex-row">
+            <div>
+                <h3>Graphical user interface</h3>
+                <p>Ensures easy navigation and minimal interaction for each analysis</p>
+            </div>
+            <div>
+                <h3>Concurrency</h3>
+                <p>Retrieval of morphological metrics is sped up by using multiple cores</p>
+            </div>
+            <div>
+                <h3>Cross platform</h3>
+                <p>Able to run on Linux, MacOS and Windows</p>
+            </div>
+        </div>
+    </div>
+</main>
